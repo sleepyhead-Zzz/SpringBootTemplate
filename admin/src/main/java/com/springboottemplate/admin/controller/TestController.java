5623082b148575d3c056958ee044a4de03f14658
Sleepyhead <realcowhorse@outlook.com>
2024-09-04 17:24:36 +0800

    ✨ 整合SpringSecurity并添加redis测试接口

--- admin/src/main/java/com/springboottemplate/admin/controller/TestController.java
+++ admin/src/main/java/com/springboottemplate/admin/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.springboottemplate.admin.controller;
 
 import com.springboottemplate.domain.system.user.db.SysUserService;
+import com.springboottemplate.infrastructure.cache.RedisUtil;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +18,12 @@ public class TestController {
     public String getConfig() {
 
         return sysUserService.list().toString();
+    }
+    @Resource
+    RedisUtil redisUtil;
+    @GetMapping("/redis")
+    public String redis(){
+        redisUtil.setCacheObject("test","test");
+        return redisUtil.getCacheObject("test").toString();
     }
 }
